0bc01a1969337c0dfa5f372265d9e431ed21fe50
unknown <cannicam@gmail.com>
2020-10-23 14:59:56 -0500

    new file

---
+++ varExer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace varExer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
