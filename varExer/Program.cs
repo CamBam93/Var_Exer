1673ab70a2c79fb4ac48daad3facde2b7b9ad2d8
unknown <cannicam@gmail.com>
2020-10-26 10:07:47 -0500

    finished file

--- varExer/Program.cs
+++ varExer/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace varExer
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        { 
+            string name = "Cam"
+            int age = 27
+            char grade = "A"
+            bool isAdult = x >= 18
+            double change = 2.75
+            decimal GPA = 3.4
+
         }
     }
 }
